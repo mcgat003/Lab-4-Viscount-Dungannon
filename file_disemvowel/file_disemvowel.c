5a40480b7051bb452c7776916fdd588d2c456714
Ian McGathey <mcgat003@neoprene.morris.umn.edu>
2013-10-10 17:29:13 -0500

    finished and tested file_disemvowel

--- file_disemvowel/file_disemvowel.c
+++ file_disemvowel/file_disemvowel.c
@@ -6,21 +6,19 @@
 #define BUF_SIZE 1024
 
 //Returns true if input character is a vowel
-bool is_vowel( char *c){
-  int result;
+bool is_vowel( char c){
   if ( c == 'A' || c == 'a' || c == 'E' || c == 'e' || c == 'I' ||  c == 'i' || c == 'O' ||  c == 'o' ||  c == 'U' ||  c == 'u'){
     return true;
-  }
-  else { 
+  }else{ 
     return false;
   }
 }
 
 int copy_non_vowels(int num_chars, char* in_buf, char*  out_buf){
   int counter = 0;
-  int i = 0;
-  for(i ; i < num_chars ; i++){
-    if(is_vowel(in_buf[i] == false)){
+  int i;
+  for( i = 0 ; i < num_chars ; i++){
+    if(is_vowel(in_buf[i]) == false){
       out_buf[counter] = in_buf[i];
       counter++; 
     }
@@ -31,7 +29,17 @@ int copy_non_vowels(int num_chars, char* in_buf, char*  out_buf){
 void disemvowel(FILE* input, FILE* output){
   int size = 0;
   char* in_buf = calloc(BUF_SIZE, sizeof(char));
-  
+  fread(in_buf, sizeof(char), BUF_SIZE, input);
+  char indexChar = 'c';
+  //While loop calculates the size of the input after it is read
+  //from the file and put into the buffer
+  while (indexChar !=  NULL){
+    indexChar = in_buf[size];
+    size++;
+  }
+  char *out_buf = calloc(size-1, sizeof(char));
+  int consCount = copy_non_vowels(size-1, in_buf, out_buf);
+  fwrite(out_buf, sizeof(char), consCount, output);
 }
 
 int main( int argc, char *argv[]){
--- file_disemvowel/file_disemvowel.h
+++ file_disemvowel/file_disemvowel.h
@@ -3,5 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main( int argc, char *argv[])
-bool is_vowel(char *c)
+
+bool is_vowel(char c);
+int copy_non_vowels(int num_chars, char* in_buf, char* out_buf);
+void disemvowel(FILE* input, FILE* output);
+int main( int argc, char *argv[]);
